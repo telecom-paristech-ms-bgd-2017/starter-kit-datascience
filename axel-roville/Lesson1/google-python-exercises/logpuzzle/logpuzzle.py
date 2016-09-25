ed8a1a0d39bfe00e839ed0e79837441943e7a934
Axel Rovillé <axel.roville@gmail.com>
2016-09-25 19:00:35 +0200

    Commenting code in logpuzzle

--- axel-roville/Lesson1/google-python-exercises/logpuzzle/logpuzzle.py
+++ axel-roville/Lesson1/google-python-exercises/logpuzzle/logpuzzle.py
@@ -18,11 +18,13 @@ Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
+# Match any string like the one above
 reg = '\d+\.\d+\.\d+\.\d+ \- \- \[.+\] \"GET (.+puzzle.+) HTTP\/1\.0\".+'
 regex_url = re.compile(reg)
 
-reg_domain = '.*[\/](.+)'
-regex_domain = re.compile(reg_domain)
+# Find the last '/' delimited part, typically the name of the file
+reg_filename = '.*[\/](.+)'
+regex_filename = re.compile(reg_filename)
 
 
 def read_urls(filename):
@@ -32,15 +34,20 @@ def read_urls(filename):
     increasing order."""
 
     urls = []
+
+    # Read the file line by line
     for line in open(filename, 'r'):
+        # Compare the line to our computed regex
         url_search_result = regex_url.search(line)
+
+        # If we found a match, add the corresponding URL
         if url_search_result:
             urls.append('http://code.google.com/' + url_search_result.group(1))
 
     return urls
 
 def filename_from_url(url):
-    return regex_domain.search(url).group(1)
+    return regex_filename.search(url).group(1)
 
 
 def download_images(img_urls, dest_dir):
@@ -51,6 +58,17 @@ def download_images(img_urls, dest_dir):
     with an img tag to show each local image file.
     Creates the directory if necessary.
     """
+
+    # If the folder doesn't exist, create it
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    # Make sure that we include the folder name in the path,
+    # and not the name of the files
+    if dest_dir[-1] != '/':
+        dest_dir += '/'
+
+    # Download and save the images in the folder
     for url in img_urls:
         urllib.request.urlretrieve(url, dest_dir + filename_from_url(url))
 
