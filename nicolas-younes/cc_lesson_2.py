0817cda167542eca44e62b670377476d2eed7f3d
NicoYounes <younes.nicolas@gmail.com>
2016-09-30 14:34:20 +0200

    Commit cc_lesson2.py

--- nicolas-younes/cc_lesson_2.py
+++ nicolas-younes/cc_lesson_2.py
@@ -9,8 +9,9 @@ def string_times(string, n):
     result = ""
     while i <= n:
         result += string
-        i = i+1
+        i += 1
     return result
+
 
 # Given an array of ints, return True if one of the first 4 elements
 # in the array is a 9. The array length may be less than 4.
@@ -34,47 +35,49 @@ def last2(string):
     i = 0
     while i < len(string) - 1:
         if string[i]+string[i+1] == subs:
-            count = count + 1
-        i = i + 1
+            count += 1
+        i += 1
     return count - 1
 
 
-#Write a program that maps a list of words into a list of
-#integers representing the lengths of the correponding words.
+# Write a program that maps a list of words into a list of
+# integers representing the lengths of the correponding words.
 def length_words(array):
     result = []
     for s in array:
         result.append(len(s))
     return result
 
-#write fizbuzz programm
+
+# write fizbuzz programm
 def fizbuzz():
     i = 0
     while i <= 100:
-        if i%3 == 0:
-            if i%5 == 0:
+        if i % 3 == 0:
+            if i % 5 == 0:
                 print("fizzbuzz")
             else:
                 print("fizz")
         else:
             print("buzz")
-        i = i + 1
+        i += 1
         print("\n")
     return
 
-#Write a function that takes a number and returns a list of its digits.
+
+# Write a function that takes a number and returns a list of its digits.
 def number2digits(number):
     result = []
-    for c in str(number) :
+    for c in str(number):
         result.append(int(c))
     return result
 
-#Write function that translates a text to Pig Latin and back.
-#English is translated to Pig Latin by taking the first letter of every word,
-#moving it to the end of the word and adding 'ay'
-def pigLatin(text):
+
+# Write function that translates a text to Pig Latin and back.
+# English is translated to Pig Latin by taking the first letter of every word,
+# moving it to the end of the word and adding 'ay'
+def piglatin(text):
     temp = ""
-    result = ""
     for w in text.lower().split():
         temp += w[1:] + w[0] + "ay "
     result = temp[:-1]
@@ -85,31 +88,30 @@ def pigLatin(text):
 class Lesson1Tests(unittest.TestCase):
 
     def testArrayFront9(self):
-        self.assertEqual(array_front9([1, 2, 9, 3, 4]) , True)
-        self.assertEqual(array_front9([1, 2, 3, 4, 9]) , False)
-        self.assertEqual(array_front9([1, 2, 3, 4, 5]) , False)
+        self.assertEqual(array_front9([1, 2, 9, 3, 4]), True)
+        self.assertEqual(array_front9([1, 2, 3, 4, 9]), False)
+        self.assertEqual(array_front9([1, 2, 3, 4, 5]), False)
 
     def testStringTimes(self):
-        self.assertEqual(string_times('Hel', 2),'HelHel' )
-        self.assertEqual(string_times('Toto', 1),'Toto' )
-        self.assertEqual(string_times('P', 4),'PPPP' )
+        self.assertEqual(string_times('Hel', 2), 'HelHel')
+        self.assertEqual(string_times('Toto', 1), 'Toto')
+        self.assertEqual(string_times('P', 4), 'PPPP')
 
     def testLast2(self):
-        self.assertEqual(last2('hixxhi') , 1)
-        self.assertEqual(last2('xaxxaxaxx') , 1)
-        self.assertEqual(last2('axxxaaxx') , 2)
+        self.assertEqual(last2('hixxhi'), 1)
+        self.assertEqual(last2('xaxxaxaxx'), 1)
+        self.assertEqual(last2('axxxaaxx'), 2)
 
     def testLengthWord(self):
-        self.assertEqual(length_words(['hello','toto']) , [5,4])
-        self.assertEqual(length_words(['s','ss','59fk','flkj3']) , [1,2,4,5])
+        self.assertEqual(length_words(['hello', 'toto']), [5, 4])
+        self.assertEqual(length_words(['s', 'ss', '59fk', 'flkj3']), [1, 2, 4, 5])
 
     def testNumber2Digits(self):
-        self.assertEqual(number2digits(8849) , [8,8,4,9])
-        self.assertEqual(number2digits(4985098) , [4,9,8,5,0,9,8])
+        self.assertEqual(number2digits(8849), [8, 8, 4, 9])
+        self.assertEqual(number2digits(4985098), [4, 9, 8, 5, 0, 9, 8])
 
     def testPigLatin(self):
-        self.assertEqual(pigLatin("The quick brown fox"), "hetay uickqay rownbay oxfay")
-
+        self.assertEqual(piglatin("The quick brown fox"), "hetay uickqay rownbay oxfay")
 
 
 def main():
