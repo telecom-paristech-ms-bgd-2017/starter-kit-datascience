fbb808c0e85d269f32d9e6bd0e7e5b2d44fa98af
Gilles Legoux <gilles.legoux@gmail.com>
2016-10-01 13:20:40 +0200

    Update cc_lesson2.py

--- gilles-legoux/Lesson2/cc_lesson2.py
+++ gilles-legoux/Lesson2/cc_lesson2.py
@@ -21,7 +21,7 @@ def array_front9(nums):
 
 
 # Given a string, return the count of the number of times
-# that a substring length 2 appears  in the string and also as
+# that a substring length 2 appears in the string and also as
 # the last 2 chars of the string, so "hixxxhi" yields 1
 # (we won't count the end substring).
 def last2(string):
