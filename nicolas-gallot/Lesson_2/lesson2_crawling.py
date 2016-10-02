9b0c233154ca80d9628dff4cc385fa79c25a1e02
ngallot <nicolas.gallot@gmail.com>
2016-10-02 12:21:28 +0200

    Add cc_lesson_2.py + copy of crawling

--- nicolas-gallot/Lesson_2/lesson2_crawling.py
+++ nicolas-gallot/Lesson_2/lesson2_crawling.py
@@ -22,13 +22,13 @@ def computeIndicatorForPage(url):
   indicator = 1000.* (number_of_likes - number_of_dislikes ) / number_of_views
 
   title = soup.title.text
-  print '====='
-  print title
-  print "Likes", number_of_likes
-  print "Dislikes", number_of_dislikes
-  print "VIews", number_of_views
-  print "Popularity", indicator
-  print '====='
+  print ('=====')
+  print (title)
+  print ("Likes", number_of_likes)
+  print ("Dislikes", number_of_dislikes)
+  print ("VIews", number_of_views)
+  print ("Popularity", indicator)
+  print ('=====')
   metrics = {}
   metrics['number_of_views'] = number_of_views
   metrics['number_of_likes'] = number_of_likes
