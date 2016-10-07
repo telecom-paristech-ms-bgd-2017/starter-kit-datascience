29b5ba5ece1058cf5fc0e441d2bcdaf75b68e59e
Omar Kanan <omar.kanan@hotmail.fr>
2016-10-07 14:04:44 +0200

    pep8

--- omar-kanan/Lesson3/exo_cc_lesson_3.py
+++ omar-kanan/Lesson3/exo_cc_lesson_3.py
@@ -2,13 +2,13 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 url_incomplete = 'http://www.cdiscount.com/search/10/ordinateur'
-end_url = '.html#_his_'
+end_of_url = '.html#_his_'
 brands = ['acer', 'dell']
 data = {'acer': [], 'dell': []}
 results = {}
 
 for brand in brands:
-    request = requests.get(url_incomplete + brand + end_url)
+    request = requests.get(url_incomplete + brand + end_of_url)
     soup = bs(request.text, 'html.parser')
 
     number_of_pcs = len(soup.select("li"))
