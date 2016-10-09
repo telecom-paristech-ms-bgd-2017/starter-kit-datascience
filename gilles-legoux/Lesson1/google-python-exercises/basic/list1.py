94f6554fceda480a2494d0d644f27eb9c52ee7a6
glegoux <gilles.legoux@gmail.com>
2016-10-09 22:05:40 +0200

    Fix branch

--- gilles-legoux/Lesson1/google-python-exercises/basic/list1.py
+++ gilles-legoux/Lesson1/google-python-exercises/basic/list1.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python -tt
+# basic/list1.py
+
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -15,14 +17,25 @@
 # It's ok if you do not complete all the functions, and there
 # are some additional functions to try in list2.py.
 
+
 # A. match_ends
 # Given a list of strings, return the count of the number of
 # strings where the string length is 2 or more and the first
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
+    count = 0
+    for word in words:
+        if len(word) < 2:
+            continue
+        if word[0] == word[-1]:
+            count += 1
+    return count
+
+
+# short but copy list one time
+def match_ends_bis(words):
+    return len(filter(lambda x: len(x) >= 2 and x[0] == x[-1], words))
 
 
 # B. front_x
@@ -33,9 +46,24 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return
+    xs = []
+    nxs = []
+    for word in words:
+        if len(word) == 0:
+            continue
+        if word[0] == 'x':
+            xs.append(word)
+        else:
+            nxs.append(word)
+    xs.sort()
+    nxs.sort()
+    return xs + nxs
 
+
+# short but copy list two times
+def front_x_bis(words):
+    return sorted(filter(lambda x: len(x) > 0 and x[0] == 'x', words)) \
+           + sorted(filter(lambda x: len(x) == 0 or x[0] != 'x', words))
 
 
 # C. sort_last
@@ -45,46 +73,59 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-  # +++your code here+++
-  return
+    ts = []
+    for k, tuple in enumerate(tuples):
+        ts.append((tuple[-1], k))
+    ts.sort()
+    tts = []
+    for k in range(len(ts)):
+        tts.append(tuples[ts[k][1]])
+    return tts
+
+
+# with standard library so easy
+def sort_last_bis(tuples):
+    return sorted(tuples, key=lambda x: x[-1])
 
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
-  if got == expected:
-    prefix = ' OK '
-  else:
-    prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'match_ends'
-  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
-  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
-  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+    for f in [match_ends, match_ends_bis]:
+        print f.__name__, ':'
+        test(f(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+        test(f(['', 'x', 'xy', 'xyx', 'xx']), 2)
+        test(f(['aaa', 'be', 'abc', 'hello']), 1)
 
-  print
-  print 'front_x'
-  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
-       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
-  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
-       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
-  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
-       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+    print
+    for f in [front_x, front_x_bis]:
+        print f.__name__, ':'
+        test(f(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+        test(f(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+        test(f(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
-  print
-  print 'sort_last'
-  test(sort_last([(1, 3), (3, 2), (2, 1)]),
-       [(2, 1), (3, 2), (1, 3)])
-  test(sort_last([(2, 3), (1, 2), (3, 1)]),
-       [(3, 1), (1, 2), (2, 3)])
-  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
-       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+    print
+    for f in [sort_last, sort_last_bis]:
+        print f.__name__, ':'
+        test(f([(1, 3), (3, 2), (2, 1)]),
+             [(2, 1), (3, 2), (1, 3)])
+        test(f([(2, 3), (1, 2), (3, 1)]),
+             [(3, 1), (1, 2), (2, 3)])
+        test(f([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+             [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
 
 
 if __name__ == '__main__':
-  main()
+    main()
