f887dd906e3dfe0f78c073584f49e39a5cee04a7
Axel Rovillé <axel.roville@gmail.com>
2016-10-11 16:16:11 +0200

    ZeroDivisionError management for users who do not own any repo

--- axel-roville/Lesson3/exo_dom_lesson_03.py
+++ axel-roville/Lesson3/exo_dom_lesson_03.py
@@ -26,7 +26,12 @@ def best_contributors(gist):
 def contributor_stars(contributor):
     repos = json.loads(api_get_repos(contributor))
     total_stars = sum([repo['stargazers_count'] for repo in repos])
-    return (contributor, total_stars / len(repos))
+
+    try:
+        return (contributor, total_stars / len(repos))
+    except ZeroDivisionError:
+        print("User {u} doesn't own any repo, moving on.".format(u=contributor))
+        return(contributor, 0)
 
 def add_stars(contributor_stars):
     global stars
@@ -38,24 +43,15 @@ def add_stars(contributor_stars):
 #########
 def api_get_repos(contributor):
     url = 'https://api.github.com/users/' + contributor + '/repos'
-    resp = requests.get(url, auth=(username, password))
+    resp = requests.get(url, params=token)
     if resp.status_code != 200:
         print(json.loads(resp.text)['message'])
         resp.raise_for_status()
     return resp.text
 
 def set_credentials():
-    try:
-        cred = json.loads(open('credentials.json').read())
-        global username
-        global password
-        username, password = cred['username'], cred['password']
-        if not username or not password:
-            raise Exception
-    except Exception:
-        print('You must provide your GitHub credentials in "credentials.json"')
-        print('Template: {"username":"<username>", "password":"<password>"}')
-        sys.exit(1)
+    global token
+    token = json.loads(open('credentials.json').read())
 
 def animateWaiting():
     idx = 0
@@ -94,7 +90,7 @@ def main():
 
     i = 1
     for c, avg in sorted_stars:
-        print((str(i) + ') ' + c).ljust(22, '.') + "{s:.2f}".format(s=avg))
+        print((str(i) + ') ' + c).ljust(22, '.') + ": {s:.2f}".format(s=avg))
         i += 1
 
 if __name__ == '__main__':
