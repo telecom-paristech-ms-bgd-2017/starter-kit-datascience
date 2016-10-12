d83a4ebbd5e902fd0e7c92008f6443c6f4b8de52
Sbargaoui <bargaoui.sam@gmail.com>
2016-10-12 10:07:24 +0200

    Update
    Updated versions

--- sami-bargaoui/Lesson 3/exo_cc_cours3.py
+++ sami-bargaoui/Lesson 3/exo_cc_cours3.py
@@ -7,7 +7,6 @@ def GetSoupFromUrl(url):
 
 def GetInfo(NameProduct):
     url='http://www.cdiscount.com/search/10/'+ NameProduct +'.html#_his_'
-
     print(url)
     soup = GetSoupFromUrl(url)
     tile = soup.findAll("div", { "class" : "prdtBloc" })
@@ -17,18 +16,21 @@ def GetInfo(NameProduct):
     for tiles_links in tile:
 
         for oldPrice in tiles_links.findAll("div", {"class": "prdtPrSt"}):
-            oldPrice = oldPrice.text.encode('utf-8')
-            oldPrice = oldPrice.replace(',', '.')
-            oldPrice = oldPrice.replace('\'', '')
-            oldPrice = float(oldPrice)
-            print 'old', oldPrice
+
             newPrice = tiles_links.findAll("span", {"class": "price"})[0].text.replace(u"\u20AC", ',')
             newPrice = newPrice.replace('\'', '')
             newPrice = newPrice.replace(',', '.')
             newPrice = float(newPrice)
-            print 'new', newPrice
-            sumNew = sumNew + newPrice
-            sumOld = sumOld + oldPrice
+            try :
+                oldPrice = oldPrice.text.encode('utf-8')
+                oldPrice = oldPrice.replace(',', '.')
+                oldPrice = oldPrice.replace('\'', '')
+                oldPrice = float(oldPrice)
+            except ValueError :
+                oldPrice = newPrice
+
+        sumNew = sumNew + newPrice
+        sumOld = sumOld + oldPrice
         discount = round((sumNew / sumOld)* 100,2)
 
     print 'Le pourcentage de discount de la marque', NameProduct, 'est', discount, '%'
@@ -37,3 +39,7 @@ NameProduct = 'dell'
 GetInfo(NameProduct)
 NameProduct = 'acer'
 GetInfo(NameProduct)
+NameProduct = 'hp'
+GetInfo(NameProduct)
+#NameProduct = 'apple'
+#GetInfo(NameProduct)
