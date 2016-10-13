a2f36c82eb6f4fc9c8c4dbf83052c8e8046d055c
pdamesin <pascal.damesin@telecom-paristech.fr>
2016-10-13 22:26:35 +0200

    lesson3

---
+++ pascal-damesin/lesson3/exo_dom_lesson3.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
+def getSoupUrl(url)
+result = requests.get(url)
+soup = BeautifulSoup(result.text, 'html.parser')
+return soup
+
+def extractListContributors(soup)
+for i in range(1,256):
+    contributorsTab = pd.DataFrame(soup.find_all(scope="row"))
+return contributorsTab
+
+soupUrl = getSoupUrl('https://gist.github.com/paulmillr/2657075')
+contrib_rows = extractListContributors(soupUrl)
+print(contrib_rows)
