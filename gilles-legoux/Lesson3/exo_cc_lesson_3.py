f0138f8e7e2e842da573ba2a2fc686a947e00345
glegoux <gilles.legoux@gmail.com>
2016-10-14 01:43:48 +0200

    Improvment cdiscount

--- gilles-legoux/Lesson3/exo_cc_lesson_3.py
+++ gilles-legoux/Lesson3/exo_cc_lesson_3.py
@@ -29,10 +29,10 @@ csv_indicators = 'data-cdiscount-com-indicators.csv'
 def get_all_items(brands, verbose=False):
     items = []
     for brand in brands:
-        url = get_url(brand)
-        for page in range(1, get_number_of_pages(url) + 1):
-            soup = extract_data_from_page(url, page)
-            item = get_items(soup, brand)
+        url = _get_url(brand)
+        for page in range(1, _get_number_of_pages(url) + 1):
+            soup = _extract_data_from_page(url, page)
+            item = _get_items(soup, brand)
             if verbose:
                 print(item)
             items.extend(item)
@@ -57,15 +57,15 @@ def compute_indicators_from_csv(csv):
 
 
 def compute_indicators(df):
-    dff = pandas.DataFrame.copy(df)
-    dff = dff.groupby(['brand']).agg(
-        {
-            'price': sum,
-            'old_price': sum
-        }
-    )
+    grouped = df.groupby(['brand'])
+    dff = grouped.agg({
+        'price': 'sum',
+        'old_price': 'sum'
+    })
     dff['decrease'] = dff['old_price'] - dff['price']
     dff['decrease_percentage'] = (dff['decrease'] / dff['price']) * 100
+    dff = dff.add_prefix('total_')
+    dff['number_of_articles'] = grouped.count()['price']
     return dff
 
 
@@ -76,12 +76,12 @@ def persist_indicators(df):
 
 
 # private functions
-def get_url(brand):
+def _get_url(brand):
     global url
     return url + '/search/{}+ordinateur+portable.html'.format(brand)
 
 
-def extract_data_from_page(url, page):
+def _extract_data_from_page(url, page):
     result = requests.get(url, params={'page': str(page)})
     if result.status_code == 404:
         sys.stderr.write('ERROR: no data for url {}.\n'.format(result.url))
@@ -90,78 +90,78 @@ def extract_data_from_page(url, page):
     return soup
 
 
-def get_number_of_pages(url):
+def _get_number_of_pages(url):
     result = requests.get(url)
     soup = BeautifulSoup(result.text, 'html.parser')
     return int(soup.find('div', class_='pg').find('ul') \
                .find_all('li')[-1].find('span').contents[0])
 
 
-def get_items(soup, brand):
-    url_elt = get_item_block(soup)
-    return extract_items(url_elt, brand)
+def _get_items(soup, brand):
+    url_elt = _get_items_block(soup)
+    return _extract_items(url_elt, brand)
 
 
-def get_item_block(soup):
+def _get_items_block(soup):
     return soup.find('ul', {'id': 'lpBloc'})
 
 
-def extract_items(ul_elt, brand):
+def _extract_items(ul_elt, brand):
     items = []
     for li_elt in ul_elt.find_all('li', recursive=False):
-        if not is_item(li_elt):
+        if not _is_item(li_elt):
             continue
-        item = extract_item(li_elt, brand)
+        item = _extract_item(li_elt, brand)
         if item:
             items.append(item)
     return items
 
 
-def is_item(li_elt):
+def _is_item(li_elt):
     return li_elt.has_attr('data-sku')
 
 
-def extract_item(li_elt, brand):
+def _extract_item(li_elt, brand):
     item = dict()
     item['sku'] = li_elt['data-sku']
-    item['title'] = get_title_item(li_elt)
-    if not is_correct_brand(item['title'], brand):
+    item['title'] = _get_title_item(li_elt)
+    if not _is_correct_brand(item['title'], brand):
         return None
     item['brand'] = brand
-    item['price'] = get_current_price_item(li_elt)
-    item['old_price'] = get_old_price_item(li_elt, item['price'])
+    item['price'] = _get_current_price_item(li_elt)
+    item['old_price'] = _get_old_price_item(li_elt, item['price'])
     return item
 
 
-def get_title_item(li_elt):
+def _get_title_item(li_elt):
     return li_elt.find('div', class_='prdtBTit').contents[0].strip()
 
 
-def is_correct_brand(title, brand):
+def _is_correct_brand(title, brand):
     return brand.lower() in title.lower()
 
 
-def get_old_price_item(li_elt, recent_price):
+def _get_old_price_item(li_elt, recent_price):
     price = li_elt.find('div', class_='prdtPrSt')
     if not price:
         return recent_price
     if len(price.contents) == 0:
         return recent_price
     price = price.contents[0].split(',')
-    unit_price = number(price[0])
-    dec_price = number(price[1])
+    unit_price = _number(price[0])
+    dec_price = _number(price[1])
     return unit_price + dec_price * 0.01
 
 
-def get_current_price_item(li_elt):
+def _get_current_price_item(li_elt):
     price = li_elt.find('div', class_='prdtPrice') \
         .find('span', class_='price')
-    unit_price = number(price.contents[0])
-    dec_price = number(price.find('sup').contents[0])
+    unit_price = _number(price.contents[0])
+    dec_price = _number(price.find('sup').contents[0])
     return unit_price + dec_price * 0.01
 
 
-def number(string):
+def _number(string):
     return int(''.join(re.findall('\d+', string)))
 
 
@@ -170,8 +170,7 @@ if __name__ == '__main__':
     df_items = persist_items(items)
     df_indicators = compute_indicators(df_items)
     persist_indicators(df_indicators)
-    df_indicators = pandas.DataFrame.from_csv(csv_indicators)
     print('*** Indicators for Dell and Acer ***')
     print(df_indicators)
     print('The winner (brand with decrease_percentage maximum) is ?')
-    print(df_indicators['decrease_percentage'].argmax())
+    print(df_indicators['total_decrease_percentage'].argmax())
