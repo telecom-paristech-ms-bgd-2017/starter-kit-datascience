f46ecd3d09f73d6758ee1f306b4f9eb38cf76eff
laura <hfpoi12@gmail.fr>
2016-10-13 23:15:48 +0200

    Lesson3

---
+++ laura-zhou/Lesson3/exo_dom_lesson3_top_github_stargazer.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import urllib.request
+from bs4 import BeautifulSoup 
+import requests
+import json 
+import pandas as pd
+
+
+def get_top_github():   
+    #read file
+    url = "https://gist.github.com/paulmillr/2657075"
+    s = urllib.request.urlopen(url).read()
+    #make soup
+    soup = BeautifulSoup(s,"html.parser")
+    res=soup.find_all("td") 
+    l=[]
+    #nom et pseudo apparaisse 1 ligne sur 4
+    for i in range(0,len(res),4): 
+        pseudo=res[i].find("a").text
+        l.append(pseudo)
+
+    return l
+
+
+def get_stargazers_count(pseudo):
+    access_token="b57ba6115902952fe60e2c88571480d22a9374ff"
+    headers = {"Authorization": "bearer " + access_token}
+    response = requests.get('https://api.github.com/users/' + pseudo + '/repos', headers=headers)
+    me_json = response.json() #permet de lire le Json dans une liste
+    count=0
+    try:
+        for i in range(len(me_json)):
+            count+=me_json[i]['stargazers_count']
+        return (count/len(me_json))
+    #jfrazelle profile is private, the count is null
+    except ZeroDivisionError:
+        return None
+        
+#save in a list        
+ccl1=get_top_github()  
+ccl2=[]
+
+#for top in get_top_github():
+#    print(top,get_stargazers_count(top))
+
+for top in get_top_github():
+    ccl2.append(get_stargazers_count(top))
+ 
+df=pd.DataFrame(ccl1) 
+df.columns=['github_name']
+df["avg_stargazer_per_repo"]=ccl2
+ 
+df_sorted=df.sort_values(by="avg_stargazer_per_repo", ascending=False)
+print(df_sorted)
+df_sorted.to_csv("github_stargazers.csv", sep=";", header = True)
