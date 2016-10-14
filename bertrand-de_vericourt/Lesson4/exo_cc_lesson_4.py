e5e2bfe0a7cfcda597fff8ed40ee903602acd708
verycourt <bertrand2vericourt@gmail.com>
2016-10-14 18:03:20 +0200

    Lesson4 > scrap API Google && create CSV

--- bertrand-de_vericourt/Lesson4/exo_cc_lesson_4.py
+++ bertrand-de_vericourt/Lesson4/exo_cc_lesson_4.py
@@ -10,6 +10,10 @@ import json
 # Pour teter les REGEX:
 # aller sur regex101.com (teste les regex)   ou  emailregex.com  => récupérer un cheat sheet
 #  importer la library  re
+# Utile: on peut faire les trim(), les split() etc vectoriellement (pour tous les éléments)
+# très UTILE: utiliser une REGEX dans un SERIE, on pourra utiliser la méthode .extract() afin de tout ordonner très rapidement.
+
+# transformer en String se dit "caster en String"
 
 # AMELIORATION pour chaîne de caractères avec paramètres:
 # utiliser >>>> 'maString1{}maString2'.format(LeParamètre)  >>> plutôt que 'maString1' + LeParamètre + 'maString2'
@@ -20,6 +24,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import csv
 
 # get 30 French biggest cities names
 cities_list = []
@@ -34,7 +39,7 @@ for city in cities[1:31]:
 	
 # get distances (on utilise des dataframes, ils sont plus manipulables que les matrices)
 
-allCities = pd.DataFrame(0, cities_list, cities_list)
+dfAllCities = pd.DataFrame(0, cities_list, cities_list)
 f = open('maCleGG.txt', 'r')
 
 for idx, city1 in enumerate(cities_list):
@@ -43,8 +48,8 @@ for idx, city1 in enumerate(cities_list):
 
 		try:
 
-			StartFrom = city1 + 'France'
-			GoTo = city2 + 'France'
+			StartFrom = city1 + '+France'
+			GoTo = city2 + '+France'
 
 			YOUR_API_KEY = f.readline()
 			urlDistance = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + \
@@ -52,17 +57,22 @@ for idx, city1 in enumerate(cities_list):
 
 			response = requests.get(urlDistance)
 			me_json = response.json()
+
 			# in meters
 			print('from ' + city1 + ' to ' + city2)
-			if city2 == 'Dijon': print(me_json)
 			distanceTemp = me_json['rows'][0]['elements'][0]['distance']['text']
-			allCities[city1][city2] = distanceTemp
+			dfAllCities[city1][city2] = distanceTemp
 			# in seconds
 			TimeTemp = me_json['rows'][0]['elements'][0]['duration']['text']
-			allCities[city2][city1] = TimeTemp
+			dfAllCities[city2][city1] = TimeTemp
 
 		except ValueError as e:
 
 			print('error ' + e)
 
-print(allCities)
+#print(dfAllCities)
+
+# Insert into a CSV
+
+fileName = 'Distances_villes'
+dfAllCities.to_csv(fileName)
