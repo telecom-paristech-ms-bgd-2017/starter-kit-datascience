f9dfbf9f58565f763c7060812347642d33ae49d7
ngallot <nicolas.gallot@gmail.com>
2016-10-30 18:46:04 +0100

    Update cc_lesson5

--- nicolas-gallot/Lesson_5/cc_lesson_5.py
+++ nicolas-gallot/Lesson_5/cc_lesson_5.py
@@ -1,6 +1,9 @@
 # Ibuprofene, Levothyroxine
 from bs4 import BeautifulSoup
 import requests
+import re
+import pandas as pd
+
 import json
 
 URL_SEARCH = 'http://base-donnees-publique.medicaments.gouv.fr/index.php#result'
@@ -9,10 +12,17 @@ regex_pattern = '(IBUPROFENE)\s([\w\s]+)\s(\d+)(\smg|\s%), ([a-zA-Zé ]{1,20})'
 data = {"choixRecherche": "medicament", "txtCaracteres": "ibuprofene"}
 
 
-re = requests.post(URL_SEARCH, data=data)
-bs = BeautifulSoup(re.text, 'html.parser')
+req = requests.post(URL_SEARCH, data=data)
+bs = BeautifulSoup(req.text, 'html.parser')
 
 table = bs.find('table', {'class' : 'result'})
 table_body = table.find('tbody')
-# rows = table_body.find_all('tr')
-print(table)
+rgx = re.compile(regex_pattern, re.IGNORECASE)
+rgx_all = rgx.findall(table.text)
+
+columns = ['MOLECULE', 'NAME', 'DOSAGE_VALUE', 'DOSAGE_UNIT',
+           'FORMAT']
+
+df = pd.DataFrame(data=rgx_all, columns=columns)
+
+print(df)
