9a0d33bec622ca93ba06568183a2d27d22f98a48
epasquet <emmanuel.n.pasquet@gmail.com>
2016-11-25 16:02:10 +0100

    Update exo_cc_lesson_6.py

--- emmanuel-pasquet/Lesson6/exo_cc_lesson_6.py
+++ emmanuel-pasquet/Lesson6/exo_cc_lesson_6.py
@@ -1,1 +1,24 @@
-z
+import pandas as pd
+import numpy as np
+
+fichier = '/home/nux/Documents/INFMDI721/Aliments/aliments.csv'
+rawDF = pd.read_csv(fichier, sep='\t')
+
+rawDF.head()
+
+rawDF.columns[0:100]
+
+sugarDF = rawDF[['countries_fr', 'sugars_100g']].dropna()
+sugarDF = sugarDF[sugarDF['sugars_100g'] >= 0.]
+sugarDF = sugarDF[sugarDF['sugars_100g'] <= 100.]
+sugarDF = sugarDF[sugarDF['countries_fr'].str.contains('France')]
+sugarDF.head(100)
+
+sugarDF['cat_sugar'] = pd.cut(sugarDF['sugars_100g'], 10)
+sugarDF.head()
+sugarDF['cat_sugar'].value_counts()
+
+sugarDF['cat_sugar'].value_counts()
+sugarDF[sugarDF['cat_sugar'].str.endswith('10]')]
+impact = 1 - float(len(sugarDF[sugarDF['cat_sugar'].str.endswith('10]')])) / len(sugarDF)
+print 'The impact is of :', impact, '%'
