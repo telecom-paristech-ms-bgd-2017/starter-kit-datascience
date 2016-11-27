7bc40c18bcf869a3bb4b9daf8aa81b8ab8bc5ee1
sandre2016 <stephan.axel.andre@gmail.com>
2016-11-27 12:13:01 +0100

    update

--- stephan-andre/Lesson5/exo_cc_lesson5.py
+++ stephan-andre/Lesson5/exo_cc_lesson5.py
@@ -5,50 +5,28 @@ Created on Thu Oct 20 18:25:49 2016
 @author: Stephan
 """
 
-from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-import numpy as np
+from bs4 import BeautifulSoup
 import re
 
-def getContentMain(page, molecule):
-    molecules = ["IBUPROFENE"]
+def get_medicam(medicam):
+    results = requests.post("http://base-donnees-publique.medicaments.gouv.fr/index.php", data = {'choixRecherche': 'medicament', 'txtCaracteres': medicam, 'action': 'show'})
+    soup = BeautifulSoup(results.text, "lxml")
+    names_medicam = soup.findAll('td', {'class':'ResultRowDeno'})
+    liste_medicam = []
+    for name in names_medicam:
+        liste_medicam.append(name.text)
+    return liste_medicam
+ 
+ibupro = get_medicam('IBUPROFENE')   
+#print(get_medicam('IBUPROFENE'))   
     
-    result = requests.post("http://base-donnees-publique.medicaments.gouv.fr/index.php#", data=
-    {  
-   		"page":page,
-   		"affliste":0,
-   		"affNumero":0,
-   		"isAlphabet":0,
-   		"inClauseSubst":0,
-   		"nomSubstances":"",
-   		"typeRecherche":0,
-   		"choixRecherche":"medicament",
-   		"paginationUsed":0,
-   		"txtCaracteres":molecule.lower(),
-   		"btnMedic.x":0,
-   		"btnMedic.y":0,
-   		"btnMedic":"Rechercher",
-   		"radLibelle":2,
-   		"radLibelleSub":4
-    })
-    soup = BeautifulSoup(result.text, 'html.parser')
-    drogList = soup.find_all(class_="standart")
+ibuprofene = []
 
-    return drogList
-
-def extractDataFromDOMMain(soup):
-    list = []
-    expression = r"IBUPROFENE\s([\w\s]+)\s(\d+)\s?([a-zA-Z%]+), ([\w\sé])"
-#    name2 = []
-#    expression2 = r"^IBUPROFENE?[A-Z]{}"
-    res_str = soup.find_all(class_="ResultRowDeno")
-    for element in res_str:        
-        while re_search(res_str, expression) is None:
-            name.append('IBUPROFENE')
-#        while re_search(res_str, expression2) is None:
-#            name2.append()
-    return name            
-
-data = getContentMain()
-result = extractDataFromDOMMain(data)
+for ibup in ibupro:
+    temp = (re.findall(r'(.*)\s(\d{1,4})\s(mg?)(?:\/\d+ mg?)?\s?(.*),\s(\S*)\s(\S*)', ibup))
+    ibuprofene.append(temp)
+    
+for i in range(len(ibuprofene)):
+    if ibuprofene[i] != []:
+        print(ibuprofene[i][0][0] + ibuprofene[i][0][1] +' '+ ibuprofene[i][0][2] +' '+ ibuprofene[i][0][3])
