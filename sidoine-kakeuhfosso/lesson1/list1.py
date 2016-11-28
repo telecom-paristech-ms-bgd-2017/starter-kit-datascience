b4f5623545c58bc66a03fb097e885987f6232b5c
sidoinis <oniskakeuh@gmail.com>
2016-11-28 18:52:06 +0200

    updating lesson1 files

--- sidoine-kakeuhfosso/lesson1/list1.py
+++ sidoine-kakeuhfosso/lesson1/list1.py
@@ -20,6 +20,7 @@
 # strings where the string length is 2 or more and the first
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
+
 def match_ends(words):
   # +++your code here+++
   cpt = 0
@@ -61,10 +62,10 @@ def front_x(words):
 #fonction qui retourne le deuxieme element de chaque tuple pour la passer a key
 def getkey(tup):
      return tup[1]
-     
+
 def sort_last(tuples):
 
-  return sorted(tuples, key=getkey)                             
+  return sorted(tuples, key=getkey)
 
 
 # Simple provided test() function used in main() to print
@@ -74,18 +75,18 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'match_ends'
+  print ('match_ends')
   test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
   test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
   test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
   print
-  print 'front_x'
+  print ('front_x')
   test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
        ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
   test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
@@ -95,7 +96,7 @@ def main():
 
 
   print
-  print 'sort_last'
+  print ('sort_last')
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
        [(2, 1), (3, 2), (1, 3)])
   test(sort_last([(2, 3), (1, 2), (3, 1)]),
