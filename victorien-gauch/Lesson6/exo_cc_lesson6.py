40486a4ff361f5813427446d2ea0e79971f26e5b
vico92 <victorien.gauch@gmail.com>
2016-11-29 22:06:09 +0100

    aliments

--- victorien-gauch/Lesson6/exo_cc_lesson6.py
+++ victorien-gauch/Lesson6/exo_cc_lesson6.py
@@ -20,26 +20,26 @@ sodium = sodium.sort_values('sodium_100g', ascending=False)
 print(sodium[['product_name', 'sodium_100g']].head(20))
 
 # On cherche les aliments avec un taux de graisse de moins de 100
-fat= df[df['fat_100g'] <= 100]
-fat= fat.sort_values('fat_100g', ascending=False)
+fat = df[df['fat_100g'] <= 100]
+fat = fat.sort_values('fat_100g', ascending=False)
 print(fat[['product_name','fat_100g']].head(20))
 
 # On cherche les aliments avec un taux de cholesterol de moins de 100%
-fat= df[df['cholesterol_100g'] <= 1]
-fat= fat.sort_values('cholesterol_100g', ascending=False)
-print(fat[['product_name','cholesterol_100g']].head(20))
+cholesterol = df[df['cholesterol_100g'] <= 1]
+cholesterol = cholesterol.sort_values('cholesterol_100g', ascending=False)
+print(cholesterol[['product_name','cholesterol_100g']].head(20))
 
 
 print()
 print('##### Pays qui fabriquent les aliments les plus gras #####')
-origins_fat = df[['countries', 'fat_100g']]
-origins_fat = origins_fat[origins_fat['countries'] != 0]
-print(origins_fat['countries'].value_counts().head())
+countries_fat = df[['countries', 'fat_100g']]
+countries_fat = countries_fat[countries_fat['countries'] != 0]
+print(countries_fat['countries'].value_counts().head())
 
 print()
 print('##### Pays qui fabriquent les aliments les plus sucrés #####')
-origins_sugars = df[['countries', 'sugars_100g']]
-origins_sugars = origins_sugars[origins_sugars['countries'] != 0]
-print(origins_sugars['countries'].value_counts().head())
+countries_sugars = df[['countries', 'sugars_100g']]
+countries_sugars = countries_sugars[countries_sugars['countries'] != 0]
+print(countries_sugars['countries'].value_counts().head())
 
 
