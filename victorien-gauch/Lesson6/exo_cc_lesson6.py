4500b043e57c3d2eaa468b49f14cbe00c4718cb6
vico92 <victorien.gauch@gmail.com>
2016-11-29 21:53:15 +0100

    aliments

--- victorien-gauch/Lesson6/exo_cc_lesson6.py
+++ victorien-gauch/Lesson6/exo_cc_lesson6.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('aliments.csv', '\t', low_memory=False)
+
+#On remplace les cases vides par des 0
 df = df.fillna(0)
-#Affiche toutes les colonnes pour pouvoir repérer celles qui nous intéressent.
+
+#On affiche toutes les colonnes pour repérer celles qui nous intéressent.
 columns = list(df.columns.values)
 print(columns)
 
@@ -17,24 +20,26 @@ sodium = sodium.sort_values('sodium_100g', ascending=False)
 print(sodium[['product_name', 'sodium_100g']].head(20))
 
 # On cherche les aliments avec un taux de graisse de moins de 100
-fat= df[df['fat_100g'] > 100]
+fat= df[df['fat_100g'] <= 100]
 fat= fat.sort_values('fat_100g', ascending=False)
 print(fat[['product_name','fat_100g']].head(20))
 
-# On compte le nombre d'éléments qui ont un packaging et on les groupent par type de packaging
-df_packaging = df['packaging'].value_counts()
-print(df_packaging.head(20))
+# On cherche les aliments avec un taux de cholesterol de moins de 100%
+fat= df[df['cholesterol_100g'] <= 1]
+fat= fat.sort_values('cholesterol_100g', ascending=False)
+print(fat[['product_name','cholesterol_100g']].head(20))
+
 
 print()
 print('##### Pays qui fabriquent les aliments les plus gras #####')
-origins_fat = df[['origins', 'fat_100g']]
-origins_fat = origins_fat[origins_fat['origins'] != 0]
-print(origins_fat['origins'].value_counts().head())
+origins_fat = df[['countries', 'fat_100g']]
+origins_fat = origins_fat[origins_fat['countries'] != 0]
+print(origins_fat['countries'].value_counts().head())
 
 print()
 print('##### Pays qui fabriquent les aliments les plus sucrés #####')
-origins_sugars = df[['origins', 'sugars_100g']]
-origins_sugars = origins_sugars[origins_sugars['origins'] != 0]
-print(origins_sugars['origins'].value_counts().head())
+origins_sugars = df[['countries', 'sugars_100g']]
+origins_sugars = origins_sugars[origins_sugars['countries'] != 0]
+print(origins_sugars['countries'].value_counts().head())
 
 
