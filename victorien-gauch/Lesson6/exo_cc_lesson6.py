d90893cc038f4f6bac4d6c080c841f072e2506f8
vico92 <victorien.gauch@gmail.com>
2016-11-29 21:07:35 +0100

    aliments

--- victorien-gauch/Lesson6/exo_cc_lesson6.py
+++ victorien-gauch/Lesson6/exo_cc_lesson6.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('aliments.csv', '\t', low_memory=False)
-
+df = df.fillna(0)
 #Affiche toutes les colonnes pour pouvoir repérer celles qui nous intéressent.
 columns = list(df.columns.values)
 print(columns)
@@ -17,27 +17,24 @@ sodium = sodium.sort_values('sodium_100g', ascending=False)
 print(sodium[['product_name', 'sodium_100g']].head(20))
 
 # On cherche les aliments avec un taux de graisse de moins de 100
-fat= df[df['fat_100g'] <= 100]
+fat= df[df['fat_100g'] > 100]
 fat= fat.sort_values('fat_100g', ascending=False)
 print(fat[['product_name','fat_100g']].head(20))
-
-# On cherche les aliments un taux d'alcool supérieur à 40°
-alcohol = df[df['alcohol_100g'] >= 40]
-alcohol = alcohol.sort_values('alcohol_100g', ascending=False)
-print(alcohol[['product_name','alcohol_100g']].head(20))
-
-# On cherche les aliments avec un taux de graisse saturées de moins de 100
-satured_fat = df[df['saturated-fat_100g'] <= 100]
-satured_fat= satured_fat.sort_values('saturated-fat_100g', ascending=False)
-print(satured_fat[['product_name','saturated-fat_100g']].head(20))
-
-# On cherche les aliments avec un taux de taurine de moins de 0.5%
-taurine = df[df['taurine_100g'] < 0.5]
-taurine = taurine.sort_values('taurine_100g', ascending=False)
-print(taurine[['product_name','taurine_100g']].head(20))
 
 # On compte le nombre d'éléments qui ont un packaging et on les groupent par type de packaging
 df_packaging = df['packaging'].value_counts()
 print(df_packaging.head(20))
+
+print()
+print('##### Pays qui fabriquent les aliments les plus gras #####')
+origins_fat = df[['origins', 'fat_100g']]
+origins_fat = origins_fat[origins_fat['origins'] != 0]
+print(origins_fat['origins'].value_counts().head())
+
+print()
+print('##### Pays qui fabriquent les aliments les plus sucrés #####')
+origins_sugars = df[['origins', 'sugars_100g']]
+origins_sugars = origins_sugars[origins_sugars['origins'] != 0]
+print(origins_sugars['origins'].value_counts().head())
 
 
